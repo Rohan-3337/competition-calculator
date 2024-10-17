283db0c67684bda73962dfc66697fb8077d873dd
Rohan-3337 <durgawati7275@gmail.com>
2024-10-17 20:12:05 +0530

    first commit

---
+++ claculator.py
@@ -0,0 +1,71 @@
+
+total =0
+flag= True
+def sum(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+def multiply(a,b):
+    return a*b
+def divide(a,b):
+    return a/b
+def header():
+    global flag,total
+    if flag:
+
+        a = int(input("Enter a number"))
+        b = int(input("Enter b number"))
+        print("Select Calulator options:")
+        print("1. Addition")
+        print("2. subtract")
+        print("3. multiply")
+        print("4. divide")
+        option = int(input("Select option:"))
+
+    else:
+        print("Select Calulator options:")
+        print("1. Addition")
+        print("2. subtract")
+        print("3. multiply")
+        print("4. divide")
+        option = int(input("Select option:-"))
+
+        a = int(input("Enter a number:-"))
+
+    
+    
+    if option == 1:
+        if flag == True:
+            total = sum(a, b)
+        else:
+            total+=a
+        flag = False
+        print(total)
+    elif option == 2:
+        if flag == True:
+            total = sub(a, b)
+        else:
+            total-=a
+        flag = False
+        print(total)
+    elif option == 3:
+        if flag == True:
+            total = multiply(a, b)
+        else:
+            total*=a
+        flag = False
+        print(total)
+    elif option == 4:
+        if flag == True:
+            total = divide(a, b)
+        else:
+            total /=a
+        flag = False
+        print(total)
+    else:
+        exit()
+    
+
+while True:
+    header()
